0b482bc83ed7130e9462aa9ed4d8498a0afadb99
Akshay Dayal <adayal@gradle.com>
2023-03-28 23:07:38 -0600

    Android ex2 lab fix

--- Android_Apps_with_Gradle_Build_Tool/exercise2/lab/app/build.gradle.kts
+++ Android_Apps_with_Gradle_Build_Tool/exercise2/lab/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
 
     buildTypes {
         release {
-            minifyEnabled = false
+            isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
