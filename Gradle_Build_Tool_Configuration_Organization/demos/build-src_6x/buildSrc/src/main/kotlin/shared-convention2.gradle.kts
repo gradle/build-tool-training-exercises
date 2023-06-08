308ded94dde9e433ab80cbb14d9554a420cad653
Akshay Dayal <adayal@gradle.com>
2023-06-08 08:40:06 -0600

    Build Org: Add task to app2 conventions

--- Gradle_Build_Tool_Configuration_Organization/demos/build-src_6x/buildSrc/src/main/kotlin/shared-convention2.gradle.kts
+++ Gradle_Build_Tool_Configuration_Organization/demos/build-src_6x/buildSrc/src/main/kotlin/shared-convention2.gradle.kts
@@ -1,3 +1,9 @@
 tasks.withType<JavaCompile>().configureEach {
     options.isDebug = false
 }
+
+tasks.register<Zip>("app2zip") {
+    from(tasks.named("test")) { include("**/*.xml") }
+    archiveFileName.set("test-results.zip")
+    destinationDirectory.set(layout.buildDirectory)
+}
